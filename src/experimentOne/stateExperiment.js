967813a133971769c5ea3cd390df516a8f76def8
JaysonD <jayson.duff@gsgus.com>
2020-08-28 16:23:27 -0400

    end of experiment one, switching between stateful component and hooks

--- src/experimentOne/stateExperiment.js
+++ src/experimentOne/stateExperiment.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./stateExperiment.css";
-// import UserInput from './components/UserInput';
 import UserOutput from "./components/UserOutput";
 
 const StateExperiment = (props) => {
@@ -11,6 +10,10 @@ const StateExperiment = (props) => {
       { name: "Frank", nickname: "Hosan" },
     ],
   });
+
+  const [bonusState, setBonusState] = useState('this is the bonus state')
+
+  console.log(namesState, bonusState)
 
   const switchUsernameHandler = () => {
     setNamesState({
@@ -40,37 +43,6 @@ const StateExperiment = (props) => {
           name={namesState.names[1].name}
           nickname={namesState.names[1].nickname}
         />
-        <li>
-          Output multiple UserOutput components in the App component (any
-          paragraph texts of your choice)
-        </li>
-        <li>
-          Pass a username (of your choice) to UserOutput via props and display
-          it there
-        </li>
-        <li>
-          Add state to the App component (=&gt; the username) and pass the
-          username to the UserOutput component
-        </li>
-        <li>
-          Add a method to manipulate the state (=&gt; an event-handler method)
-        </li>
-        <li>
-          Pass the event-handler method reference to the UserInput component and
-          bind it to the input-change event
-        </li>
-        <li>
-          Ensure that the new input entered by the user overwrites the old
-          username passed to UserOutput
-        </li>
-        <li>
-          Add two-way-binding to your input (in UserInput) to also display the
-          starting username
-        </li>
-        <li>
-          Add styling of your choice to your components/ elements in the
-          components - both with inline styles and stylesheets
-        </li>
       </ol>
     </div>
   );
@@ -78,19 +50,4 @@ const StateExperiment = (props) => {
 
 export default StateExperiment;
 
-// class StateExperiment extends Component {
-//   state = {
-//     names: [
-//       { name: 'Bob', nickname: 'Bildar'},
-//       { name: 'Jayson', nickname: 'Json'},
-//       { name: 'Frank', nickname: 'Hosan'}
-//     ]
-//   }
 
-//   switchUsernameHandler = () => {
-//     this.setState({names: [
-//       { name: 'Bo-bithia', nickname: 'Bildarius'},
-//       { name: 'Jaysoni', nickname: 'Jsoney Pepperoni'},
-//       { name: 'Franken', nickname: 'Hostein'}
-//     ] })
-//   }
