10b24a47f6f9bef8203d268bd0c08f0b9f14a226
JaysonD <jayson.duff@gsgus.com>
2020-09-03 15:33:23 -0400

    character counter working correctly

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import StateExperiment from './experimentOne/stateExperiment';
 import * as serviceWorker from './serviceWorker';
 import ListsAndConditionals from './experimentThree/listsAndConditionals';
 
@@ -10,7 +9,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
