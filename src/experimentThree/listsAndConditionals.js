4094e1bd2822cd32ed067875aa4f165ea17c76c5
JaysonD <jayson.duff@gsgus.com>
2020-09-07 11:53:12 -0400

    adding character boxes for each character of input

--- src/experimentThree/listsAndConditionals.js
+++ src/experimentThree/listsAndConditionals.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Validation from "./components/Validation";
+import Char from "./components/CharBox";
 import "./style.css";
 
 class ListsAndConditionals extends Component {
@@ -14,11 +15,16 @@ class ListsAndConditionals extends Component {
   };
 
   render() {
+    const charList = this.state.userInput.split('').map((char, index) => {
+      return <Char character={char} key={index} />
+    })
+
     return (
       <div className="body">
         <input type="text" onChange={this.inputChangedHandler} value={this.state.userInput}></input>
         <p>{this.state.userInput}</p>
         <Validation inputLength={this.state.userInput.length} />
+        {charList}
       </div>
     );
   }
