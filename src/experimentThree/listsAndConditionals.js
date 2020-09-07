72c5ccfca7b1e04ab6c6b61356d9c7b27a6f17ce
JaysonD <jayson.duff@gsgus.com>
2020-09-07 11:32:57 -0400

    renaming functions to standardize

--- src/experimentThree/listsAndConditionals.js
+++ src/experimentThree/listsAndConditionals.js
@@ -1,44 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Validation from "./components/Validation";
 import CharBox from "./components/CharBox";
-import './style.css';
+import "./style.css";
 
 class ListsAndConditionals extends Component {
   state = {
-    length: 0
+    userInput: ''
   };
 
-  characterCounter = (event) => {
-      this.setState({
-        length: event.target.value.length
-      })
-  }
+  inputChangedHandler = (event) => {
+    this.setState({
+      userInput: event.target.value
+    });
+  };
 
   deleteCharHandler = (charIndex) => {
     const chars = [...this.state.chars];
     chars.splice(charIndex, 1);
-    this.setState({chars: chars});
-  }
+    this.setState({ chars: chars });
+  };
 
   render() {
     return (
       <div className="body">
-        <ol>
-          {/* <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li> */}
-          {/* <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li> */}
-          {/* <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li> */}
-          {/* <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li> */}
-          <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
-          <li>When you click a CharComponent, it should be removed from the entered text.</li>
-        </ol>
-        {/* <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p> */}
         {this.state.length.map((char, index) => {
-          return <CharBox
-                  click={() => this.deleteCharHandler(index)} />
+          return <CharBox click={() => this.deleteCharHandler(index)} />;
         })}
-        <input type='text' onChange={this.characterCounter}></input>
+        <input type="text" onChange={this.inputChangedHandler}></input>
         <p>{this.state.length} character(s)</p>
-        <Validation length={this.state.length}/>
+        <Validation length={this.state.length} />
       </div>
     );
   }
