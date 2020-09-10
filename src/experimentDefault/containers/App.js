4671f830727d0f3c37b65d7adff44392e82f9cb6
JaysonD <jayson.duff@gsgus.com>
2020-09-10 11:20:39 -0400

    bug fixing with passing in empty array for persons

--- src/experimentDefault/containers/App.js
+++ src/experimentDefault/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import Person from "../components/Persons/Person/Person";
+import Persons from "../components/Persons/Persons";
 
 class ExperimentDefault extends Component {
   state = {
@@ -58,22 +58,16 @@ class ExperimentDefault extends Component {
       },
     };
 
-    let persons = null;
+    let persons = [];
 
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map((person, index) => {
-            return (
-                <Person
-                  click={() => this.deletePersonHandler(index)}
-                  name={person.name}
-                  age={person.age}
-                  key={person.id}
-                  changed={(event) => this.nameChangedHandler(event, person.id)}
-                />
-            );
-          })}
+          <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler}
+          />
         </div>
       );
 
