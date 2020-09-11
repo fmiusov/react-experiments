0463e4e27029568a841e7690247b6e5f3f9c7228
JaysonD <jayson.duff@gsgus.com>
2020-09-11 11:11:15 -0400

    solving dynamic styling issues in cockpit component with button

--- src/experimentDefault/components/Cockpit/Cockpit.js
+++ src/experimentDefault/components/Cockpit/Cockpit.js
@@ -2,19 +2,21 @@ import React from "react";
 import classes from "./Cockpit.css";
 
 const cockpit = (props) => {
-    const classes = [];
+    const assignedClasses = [];
+    let btnClass = '';
+    btnClass = classes.Red;
     if (props.persons.length <= 2) {
-      classes.push("red");
+      assignedClasses.push( classes.red );
     }
     if (props.persons.length <= 1) {
-      classes.push("bold");
+      assignedClasses.push( classes.bold );
     }
 
   return (
-    <div {classes.Cockpit}>
+    <div className={classes.Cockpit}>
       <h1>Hi, I'm a React App</h1>
-      <p className={classes.join(" ")}>This is really working!</p>
-      <button className="button" onClick={this.togglePersonsHandler}>
+      <p className={assignedClasses.join(" ")}>This is really working!</p>
+      <button className={btnClass} onClick={this.togglePersonsHandler}>
         Toggle Persons
       </button>
     </div>
