ab741bc40271f30c6640a9f25bfbe22be5a33be3
JaysonD <jayson.duff@gsgus.com>
2020-09-14 16:15:02 -0400

    higher order component Aux implemented

--- src/experimentDefault/components/Persons/Person/Person.js
+++ src/experimentDefault/components/Persons/Person/Person.js
@@ -1,22 +1,26 @@
 import React, { Component } from "react";
 import classes from "./Person.css";
+import Aux from "../../../hoc/Auxilliary";
 
-class Person extends Component{
-    render() {
-        console.log("[Person.js] rendering...");
-        return (
-            <div className={classes.Person}>
-              <p onClick={this.props.click}>
-                I'm {this.props.name} and I am {this.props.age} years old!
-              </p>
-              <p>{this.props.children}</p>
-              <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
-          );
-        };
-
-    }
-
-
+class Person extends Component {
+  render() {
+    console.log("[Person.js] rendering...");
+    return (
+      <Aux>
+        <div className={classes.Person}>
+          <p onClick={this.props.click}>
+            I'm {this.props.name} and I am {this.props.age} years old!
+          </p>
+          <p>{this.props.children}</p>
+          <input
+            type="text"
+            onChange={this.props.changed}
+            value={this.props.name}
+          />
+        </div>
+      </Aux>
+    );
+  }
+}
 
 export default Person;
