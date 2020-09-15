3641ba56fa810e7f3d4b37f9504faa28e22e295f
JaysonD <jayson.duff@gsgus.com>
2020-09-15 13:52:14 -0400

    prop types library

--- src/experimentDefault/components/Persons/Person/Person.js
+++ src/experimentDefault/components/Persons/Person/Person.js
@@ -11,8 +11,9 @@ class Person extends Component {
           <p onClick={this.props.click}>
             I'm {this.props.name} and I am {this.props.age} years old!
           </p>
-          <p>{this.props.children}</p>
+          <p key="i2" >{this.props.children}</p>
           <input
+            key="i3"
             type="text"
             onChange={this.props.changed}
             value={this.props.name}
