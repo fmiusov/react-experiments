597ef4cb7528a1d186b7361f158cfbbc416009e8
JaysonD <jayson.duff@gsgus.com>
2020-09-17 16:08:15 -0400

    controlling authentication with context api

--- src/experimentDefault/components/Cockpit/Cockpit.js
+++ src/experimentDefault/components/Cockpit/Cockpit.js
@@ -2,15 +2,15 @@ import React, { useEffect, useRef } from "react";
 import classes from "./Cockpit.css";
 
 const cockpit = (props) => {
-  const toggleBtnRef = useRef(null);
+  // const toggleBtnRef = useRef(null);
 
-  useEffect(() => {
-    console.log("[Cockpit.js] useEffect");
-    // setTimeout(() => {
-    //   alert("Saved data to cloud!");
-    // }, 1000);
-    toggleBtnRef.current.click();
-  }, []);
+  // useEffect(() => {
+  //   console.log("[Cockpit.js] useEffect");
+  //   // setTimeout(() => {
+  //   //   alert("Saved data to cloud!");
+  //   // }, 1000);
+  //   toggleBtnRef.current.click();
+  // }, []);
 
   const assignedClasses = [];
   let btnClass = "";
@@ -29,9 +29,9 @@ const cockpit = (props) => {
     <div className={classes.Cockpit}>
       <h1>{props.title}</h1>
       <p className={assignedClasses.join(" ")}>This is really working!</p>
-      <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
+      {/* <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}> */}
         Toggle Persons
-      </button>
+      {/* </button> */}
     </div>
   );
 };
